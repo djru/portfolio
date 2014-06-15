cc547c1ca014e2353c8056fc0483c26d268347a2
Dan Ruswick <dan.ruswick@gmail.com>
2014-06-15 14:44:56 -0500

    Added code for main content container

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,5 @@
 console.log('This would be the main JS file.');
 
 window.onload = function(){
-  document.querySelector('h1').classList.add('active');
+  document.querySelector('div#main').classList.add('active');
   };
