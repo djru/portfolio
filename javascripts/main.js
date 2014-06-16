07499a21fed1579063ef6599dd29c45c4fddc3ea
Dan Ruswick <dan.ruswick@gmail.com>
2014-06-16 16:04:39 -0500

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,20 +1,20 @@
 console.log('This would be the main JS file.');
 
 window.onscroll = function(){
-  document.querySelector('div#spalsh #header').style['margin-top'] = (window.pageYOffset * -2) + 'px';
-  document.querySelector('div#splash #arrow').style['bottom'] = (window.pageYOffset * 2 + 10) + 'px';
+  document.querySelector('#header').style['margin-top'] = (window.pageYOffset * -2) + 'px';
+  document.querySelector('#arrow').style['bottom'] = (window.pageYOffset * 2 + 10) + 'px';
   };
 
 window.onload = function(){
-  document.querySelector("div#splash").style['height'] = window.innerHeight + 'px';
-  document.querySelector("div#content").style['min-height'] = window.innerHeight + 'px';
-  document.querySelector("div#content").style['top'] = window.innerHeight + 'px';
-  document.querySelector('div#splash #header').classList.add('active');
-  document.querySelector('div#splash #arrow').style['display'] = 'block';
+  document.querySelector("#splash").style['height'] = window.innerHeight + 'px';
+  document.querySelector("#content").style['min-height'] = window.innerHeight + 'px';
+  document.querySelector("#content").style['top'] = window.innerHeight + 'px';
+  document.querySelector('#header').classList.add('active');
+  document.querySelector('#arrow').style['display'] = 'block';
   };
 
 window.onresize = function(){
-  document.querySelector("div#splash").style['height'] = window.innerHeight + 'px';
-  document.querySelector("div#content").style['min-height'] = window.innerHeight + 'px';
-  document.querySelector("div#content").style['top'] = window.innerHeight + 'px';
+  document.querySelector("#splash").style['height'] = window.innerHeight + 'px';
+  document.querySelector("#content").style['min-height'] = window.innerHeight + 'px';
+  document.querySelector("#content").style['top'] = window.innerHeight + 'px';
 }
