58443613e60ee07bf11faf803a518aff0637f6f9
Dan Ruswick <dan.ruswick@gmail.com>
2014-06-16 16:06:15 -0500

    removed arrow

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,7 +2,6 @@ console.log('This would be the main JS file.');
 
 window.onscroll = function(){
   document.querySelector('#header').style['margin-top'] = (window.pageYOffset * -2) + 'px';
-  document.querySelector('#arrow').style['bottom'] = (window.pageYOffset * 2 + 10) + 'px';
   };
 
 window.onload = function(){
@@ -10,7 +9,6 @@ window.onload = function(){
   document.querySelector("#content").style['min-height'] = window.innerHeight + 'px';
   document.querySelector("#content").style['top'] = window.innerHeight + 'px';
   document.querySelector('#header').classList.add('active');
-  document.querySelector('#arrow').style['display'] = 'block';
   };
 
 window.onresize = function(){
