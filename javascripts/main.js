eff204e4a85833d8926d1d1c6a33397a71c9aebf
Dan Ruswick <dan.ruswick@gmail.com>
2014-06-16 15:58:53 -0500

    added code for arrow

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,9 @@
 console.log('This would be the main JS file.');
 
-window.onscroll = function(){document.querySelector('#header').style['margin-top'] = (window.pageYOffset * -2) + 'px'}
+window.onscroll = function(){
+  document.querySelector('#header').style['margin-top'] = (window.pageYOffset * -2) + 'px';
+  document.querySelector('#arrow').style['padding-bottom'] = (window.pageYOffset * 2 + 10) + 'px';
+  };
 
 window.onload = function(){
   document.querySelector("div#splash").style['height'] = window.innerHeight + 'px';
