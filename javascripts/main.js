4e33367e60c950b0388424549bb6e8853d0da038
Dan Ruswick <dan.ruswick@gmail.com>
2014-06-16 16:03:04 -0500

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,15 +1,16 @@
 console.log('This would be the main JS file.');
 
 window.onscroll = function(){
-  document.querySelector('#header').style['margin-top'] = (window.pageYOffset * -2) + 'px';
-  document.querySelector('#arrow').style['bottom'] = (window.pageYOffset * 2 + 10) + 'px';
+  document.querySelector('div#spalsh #header').style['margin-top'] = (window.pageYOffset * -2) + 'px';
+  document.querySelector('div#splash #arrow').style['bottom'] = (window.pageYOffset * 2 + 10) + 'px';
   };
 
 window.onload = function(){
   document.querySelector("div#splash").style['height'] = window.innerHeight + 'px';
   document.querySelector("div#content").style['min-height'] = window.innerHeight + 'px';
   document.querySelector("div#content").style['top'] = window.innerHeight + 'px';
-  document.querySelector('div#header').classList.add('active');
+  document.querySelector('div#splash #header').classList.add('active');
+  document.querySelector('div#splash #arrow').style['display'] = 'block';
   };
 
 window.onresize = function(){
