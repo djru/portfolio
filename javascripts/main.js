b05e98ef36454171ec75432b5d279e48e590246d
Dan Ruswick <dan.ruswick@gmail.com>
2014-06-15 20:32:32 -0500

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,5 +3,7 @@ console.log('This would be the main JS file.');
 
 window.onload = function(){
   document.querySelector("div#splash").style['height'] = window.innerHeight + 'px';
+  document.querySelector("div#content").style['height'] = window.innerHeight + 'px';
+  document.querySelector("div#content").style['top'] = window.innerHeight + 'px';
   document.querySelector('div#header').classList.add('active');
   };
