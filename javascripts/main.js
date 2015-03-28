90aa495e75e4f95603349501798316181b7abc5f
Dan Ruswick <dan.ruswick@gmail.com>
2015-03-28 02:01:39 -0500

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -24,7 +24,8 @@ window.onload = function(){
   content.style['top'] = iHeight + 'px';
   content.style['min-height'] = iHeight + 'px';
   console.log(content.style['height']);
-  content.style['height'] = (m.clientHeight -  Math.floor(maxOffset/0.5) + 100) + 'px';
+  content.style['height'] = (m.clientHeight - Math.floor(maxOffset/0.5) + 100) + 'px';
+  console.log(m.clientHeight - Math.floor(maxOffset/0.5) + 100) + 'px');
   console.log(content.style['height']);
   header.style['margin-top'] = 'calc(' + Math.floor(iHeight/2) + 'px - 100px)';
   
