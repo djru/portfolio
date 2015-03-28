19b903bd32f0c712686f70254f9e018079975977
Dan Ruswick <dan.ruswick@gmail.com>
2015-03-28 01:55:39 -0500

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -23,7 +23,9 @@ window.onload = function(){
   splash.style['height'] = iHeight + 'px';
   content.style['top'] = iHeight + 'px';
   content.style['min-height'] = iHeight + 'px';
+  console.log(content.style.height);
   content.style['height'] = (main.clientHeight -  Math.floor(maxOffset/0.5) + 100) + 'px';
+  console.log(content.style.height);
   header.style['margin-top'] = 'calc(' + Math.floor(iHeight/2) + 'px - 100px)';
   
   main.classList.remove('invisible');
